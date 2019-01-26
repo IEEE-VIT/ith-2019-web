85141daad6df26943c5f75eb9d34f92dc8621673
mayankshah1607 <mayankm.shah2017@vitstudent.ac.in>
2019-01-27 00:33:45 +0530

    Fixed Parallax

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Pages/Header/Header';
+import AboutC from './Pages/AboutC/AboutC';
 import Navigation from './Components/Nav/Nav';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header/>
+      <div className='App'>
         <Navigation/>
+        <Header/>
+        <AboutC/>
       </div>
     );
   }
---
+++ src/Pages/AboutC/AboutC.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './AboutC.css'
+
+export default class AboutC extends Component{
+    render(){
+        return(
+            <div className='aboutc'>
+            </div>
+        );
+    }
+}

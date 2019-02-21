c46fdd69fc39614865d67e86ee2f064ce207ca79
mayankshah1607 <mayankshah1614@gmail.com>
2019-02-21 15:12:21 +0530

    Form ready

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -13,6 +13,7 @@ export default class Register extends Component{
             regno: '',
             city: '',
             ieee_member: false,
+            ieee_id: '',
             q1: '',
             q2: '',
             q3: ''
@@ -21,8 +22,79 @@ export default class Register extends Component{
     }
 
     onNameChange = (event) => {
-        console.log(event.target.value)
+        this.setState({name: event.target.value})
     }
+
+    onEmailChange = (event) => {
+        this.setState({email: event.target.value})
+    }
+
+    onMobChange = (event) => {
+        this.setState({mobile: event.target.value})
+    }
+
+    onUniChange = (event) => {
+        this.setState({university: event.target.value})
+    }
+
+    onRegChange = (event) => {
+        this.setState({regno: event.target.value})
+    }
+
+    onCityChange = (event) => {
+        this.setState({city: event.target.value})
+    }
+
+    onIMChange = (event) => {
+        this.setState({ieee_id: event.target.value})
+    }
+
+    onQ1Change = (event) => {
+        this.setState({q1: event.target.value})
+    }
+
+    onQ2Change = (event) => {
+        this.setState({q2: event.target.value})
+    }
+
+    onQ3Change = (event) => {
+        this.setState({q3: event.target.value})
+    }
+
+    onRegister = () => {
+        alert('The registrations will begin shortly! Come back later.')
+    }
+
+    extCheck = () => {
+        this.setState({external: !this.state.external})
+    }
+
+    ieeeCheck = () => {
+        this.setState({ieee_member: !this.state.ieee_member})
+    }
+
+    mobValidator(){
+        var mob_test = /^[0][1-9]\d{9}$|^[1-9]\d{9}$/
+        if (mob_test.test(this.state.mobile) || this.state.mobile === ''){
+            return null
+        }
+        else{
+            return 'error'
+        }
+    }
+
+    emailValidator(){
+        var email_test = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (email_test.test(this.state.email) || this.state.email === '')
+        {
+            return null
+        }
+
+        else{
+            return 'error'
+        }
+    }
+    
     render(){
         return(
             <div id='register'>
@@ -39,58 +111,58 @@ export default class Register extends Component{
                                     <FormControl onChange={this.onNameChange} placeholder='Full Name' type="text" />                
                                 </FormGroup>
 
-                                <FormGroup>
-                                    <FormControl placeholder='Email ID' type="text" />                
+                                <FormGroup validationState={this.emailValidator()}>
+                                    <FormControl onChange={this.onEmailChange} placeholder='Email ID' type="text" />                
                                 </FormGroup>
 
-                                <FormGroup >
-                                    <FormControl placeholder='Mobile Number' type="text" />                
+                                <FormGroup validationState={this.mobValidator()}>
+                                    <FormControl onChange={this.onMobChange} placeholder='Mobile Number' type="text" />             
                                 </FormGroup>
                                 
                                 <FormGroup>
-                                    <Checkbox readOnly>
+                                    <Checkbox onClick={this.extCheck} readOnly>
                                     Check this box if you are an External Student
                                     </Checkbox>
                                 </FormGroup>
 
                                 <FormGroup>
-                                    <FormControl placeholder='University / College Name' type="text" />                
+                                    <FormControl onChange={this.onUniChange} placeholder='University / College Name' type="text" />                
                                 </FormGroup>
 
                                 <FormGroup>
-                                    <FormControl placeholder='Registration Number (Not for external students)' type="text" />                
+                                    <FormControl disabled={this.state.external} onChange={this.onRegChange}  placeholder='Registration Number (Not for external students)' type="text" />                
                                 </FormGroup>
 
                                 <FormGroup>
-                                    <FormControl placeholder='City' type="text" />                
+                                    <FormControl onChange={this.onCityChange} placeholder='City' type="text" />                
                                 </FormGroup>
 
                                 <FormGroup>
-                                    <Checkbox readOnly>
+                                    <Checkbox onClick={this.ieeeCheck} readOnly>
                                     Check this box if you are an IEEE Member
                                     </Checkbox>
                                 </FormGroup>
 
                                 <FormGroup>
-                                    <FormControl placeholder='IEEE Membership ID' type="text" />                
+                                    <FormControl disabled={!this.state.ieee_member} onChange={this.onIMChange} placeholder='IEEE Membership ID' type="text" />                
                                 </FormGroup>
 
                                 <FormGroup>
                                     <ControlLabel>Why do you want to attend ITH?</ControlLabel>
-                                    <FormControl placeholder='Enter your answer' type="text" />                
+                                    <FormControl onChange={this.onQ1Change} placeholder='Enter your answer' type="text" />                
                                 </FormGroup>
 
                                 <FormGroup>
                                     <ControlLabel>Have you attended hack-a-thons before?<br/> If yes, list them.</ControlLabel>
-                                    <FormControl placeholder='Enter your answer' type="text" />                
+                                    <FormControl onChange={this.onQ2Change} placeholder='Enter your answer' type="text" />                
                                 </FormGroup>
 
                                 <FormGroup>
                                     <ControlLabel>Kindly share details of your previous projects<br/> (if any).</ControlLabel>
-                                    <FormControl placeholder='Enter your answer' type="text" />                
+                                    <FormControl onChange={this.onQ3Change} placeholder='Enter your answer' type="text" />                
                                 </FormGroup>
 
-                                <Button>Register</Button>
+                                <Button onClick={this.onRegister}>Register</Button>
                             </form>
                         </div>
                     </Col>

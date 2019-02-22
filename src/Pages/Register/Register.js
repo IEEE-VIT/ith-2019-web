7b6f0f69c49660e3fa1324c13caec3be607c0cff
mayankshah1607 <mayankshah1614@gmail.com>
2019-02-22 11:12:51 +0530

    removing cors

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -121,8 +121,7 @@ export default class Register extends Component{
                 fetch('https://ith2019-api.herokuapp.com/register',{
                     method: 'post',
                     headers: {'Content-type':'application/json'},
-                    credentials: 'include',
-                    mode: 'cors',
+                    mode: 'no-cors',
                     body: JSON.stringify(req_body)
                 })
                 .then(response => response.json())

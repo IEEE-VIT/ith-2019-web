ff09b86882bf9b6149331b8fe0e49b8652afe49e
mayankshah1607 <mayankshah1614@gmail.com>
2019-03-04 01:01:02 +0530

    Sponsors Nav item

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -48,8 +48,8 @@ export default class Navigation extends Component{
                         <NavItem href='#highlights'>HIGHLIGHTS</NavItem>
                         {/* <NavItem>ITINERARY</NavItem> */}
                         <NavItem href='https://drive.google.com/file/d/1woX2dTw-aPrfV6_beNt6QqtCGdVqv-6E/view?usp=sharing' target='_top'>BROCHURE</NavItem>
-                        {/* <NavItem>SPEAKERS</NavItem>
-                        <NavItem>SPONSORS</NavItem> */}
+                        {/* <NavItem>SPEAKERS</NavItem> */}
+                        <NavItem href='#sponsors'>SPONSORS</NavItem>
                         <NavItem href='#about-ieeevit'>IEEE-VIT</NavItem>
                         <NavItem href='#contact-us'>CONTACT US</NavItem>
                     </Nav>
